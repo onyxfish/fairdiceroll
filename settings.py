5e5125b5f1d96c63bce6a54d768ec330f699da1a
Christopher Groskopf <staringmonkey@gmail.com>
2011-03-25 20:59:51 -0500

    Strip out auth dependency.

--- settings.py
+++ settings.py
@@ -5,24 +5,20 @@ import os
 SECRET_KEY = '=r-$b*8hglm+858&9tq43hlm6-&6-3d3vfc4((7yd0dbrakhvi'
 
 INSTALLED_APPS = (
-#    'django.contrib.admin',
     'django.contrib.contenttypes',
-    'django.contrib.auth',
     'django.contrib.sessions',
     'djangotoolbox',
 
-    # djangoappengine should come last, so it can override a few manage.py commands
+    # djangoappengine should come last
     'djangoappengine',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.core.context_processors.media',
 )
