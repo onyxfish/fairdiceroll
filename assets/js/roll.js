68c49d0ff16ad211841cd16a245716ea0d60f507
Christopher Groskopf <staringmonkey@gmail.com>
2011-03-26 21:15:34 -0500

    Added basic display of recalled rolls.

--- assets/js/roll.js
+++ assets/js/roll.js
@@ -23,7 +23,61 @@ function recall_roll(id) {
 }
 
 function render_roll(data) {
-    console.log(data);
+    die = "d6";
+    number = data["d6"][0];
+
+    now = new Date();
+
+    timestamp = new Date();
+    timestamp.setTime(Date.parse(data["timestamp"]));
+
+    delta = timestamp - now;
+
+    second = 1000;
+    minute = second * 60;
+    hour = minute * 60;
+    day = hour * 24;
+
+    if (delta > day) {
+        age = Math.floor(delta / day);
+
+        if (age > 1) {
+            age += " day";
+        } else {
+            age += " days";
+        }
+    } else if (delta > hour) {
+        age = Math.floor(delta / hour);
+
+        if (age == 1) {
+            age += " hour";
+        } else {
+            age += " hours";
+        }
+    } else if (delta > minute) {
+        age = Math.floor(delta / minute);
+
+        if (age == 1) {
+            age += " minute";
+        } else {
+            age += " minutes";
+        }
+    } else if (delta > second) {
+        age = Math.floor(delta / minute);
+
+        if (age == 1) {
+            age += " second";
+        } else {
+            age += " days";
+        }
+    }
+
+    $("#results .number").text(number);
+    $("#results .die").text(die);
+    $("#results .timestamp").text(timestamp.toDateString());
+    $("#results .age").text(age);
+
+    $("#results").show();
 }
 
 $(function() {
